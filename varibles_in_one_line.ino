8c402da0a76ac7bc77e8ecf3df008a1361dc8788
ebradford <emma_bradford@milton.edu>
2024-08-06 13:55:46 -0400

    can print varibles in one line but it is annoying

---
+++ varibles_in_one_line.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  int i = 7;
+
+  Serial.print("the numeber is: ");
+  Serial.print(i);
+  Serial.println(".");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
